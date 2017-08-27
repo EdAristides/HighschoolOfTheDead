7c328d75f1e6f2950de92351f9f431b367ed398e
EdAristides <aristides1212@gmail.com>
2017-08-27 10:22:29 -0300

    Alterando botões e campos de datas

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,18 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-datepicker
+//= require jquery.inputmask
+//= require jquery.inputmask.extensions
+//= require jquery.inputmask.numeric.extensions
+//= require jquery.inputmask.date.extensions
 
 $(document).ready(function(){
-	$('.datepicker').datepicker({format: 'dd-mm-yyyy'});
-	$('#matricula').inputmask("9999.9.999.9999");
+	$('.datepicker').datepicker({format: 'yyyy-mm-dd'});
+    $('#matricula').inputmask("999999999999");
+});
+
+$(document).ready(function(){
+	$('.datepicker').datepicker({format: 'yyyy-mm-dd'});
+    $('#siape').inputmask("9999999");
 });
